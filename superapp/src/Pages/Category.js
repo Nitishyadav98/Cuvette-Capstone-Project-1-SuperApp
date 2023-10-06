24395c0a4da64751663341bd6068ba0e005f368d
Nitish Yadav <nitish.yadav0911@gmail.com>
2023-10-06 22:28:11 +0530

    added date and weather card

--- superapp/src/Pages/Category.js
+++ superapp/src/Pages/Category.js
@@ -1,13 +1,16 @@
 import '../Styles/Category.css'
 import data from '../data/dataset.json'
 import Card from '../Components/Card'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 const Category = () =>{
 
     const navigate = useNavigate()
 
     const [selectedCategories, setSelectedCategories] = useState([])
+    // useEffect(()=>{
+    //     setSelectedCategories(JSON.parse(localStorage.getItem('categories')))
+    // },[])
 
     const nextPageHandler =()=>{
         if(selectedCategories.length >= 3){
@@ -16,12 +19,19 @@ const Category = () =>{
         }
     }
 
+    const deleteCategory = (item)=>{
+        setSelectedCategories((prev)=>(prev.filter((el)=>(el !== item))))
+    }
+
     return(<div className="categorypagelayout">
         <div className='categorypage'>
         <div className="categorychosen">
             <div className='heading'>SuperApp</div>
             <div className='subheading'>Choose Your Entertainment Category</div>
-            <div className='categorycontainer'>{selectedCategories.map((el)=>(<div className='selectedcategory'>{el}</div>))}</div>
+            <div className='categorycontainer'>{selectedCategories.map((el)=>(<div className='selectedcategory'>
+                {el}
+                <button onClick={(el)=>deleteCategory(el)}>X</button>
+                </div>))}</div>
             {selectedCategories.length < 3 && <div className='warning'>Minimum 3 categories required</div>}
         </div>
         <div className="rightside">
--- superapp/src/Pages/profile.js
+++ superapp/src/Pages/profile.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import '../Styles/Profile.css'
 
 const Profile = () => {
+    const [article, setArticle]= useState()
+    const [weather, setWeather] = useState()
+
 
     const profileDetails = JSON.parse(localStorage.getItem('profileDetails'))
     const categories = JSON.parse(localStorage.getItem('categories'))
 
-  return (
+    useEffect(()=>{
+
+        const weatherdetails = async()=>{
+            const response = await fetch('http://api.weatherapi.com/v1/current.json?key=662e6591938c4b5b922182328230510&q=London')
+            const data = await response.json()
+            setWeather(data.current)
+            // console.log(weather)
+        }
+    
+    const newsdata = async() =>{
+        const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=0c4847d1828341f280e7570c5dbdd567')
+        const data = await res.json()
+        console.log(data)        
+        // setArticle(data.articles[1])
+        // console.log(article)
+    }
+    weatherdetails();
+    // newsdata()
+    },[])
+
+    // useEffect(()=>{
+    //     console.log(article)
+    // },[weather])
+    
+    return (
     <div className='profilePage'>
         <div className='profileSection'>
             <div className='profileCard'>
@@ -19,11 +46,25 @@ const Profile = () => {
                 </div>
             </div>
             <div className='weatherSection'>
-                Weather Section
+                <div className='dateandtime'>date and time</div>
+                <div className='weatherdetails'>
+                    <div>
+                        <div><img src={weather && weather.condition.icon}/></div>
+                        <div>{weather && weather.condition.text}</div>
+                    </div>
+                    <div>
+                        <div>{weather && weather.temp_c} C</div>
+                        <div>{weather && weather.pressure_mb} mb pressure</div>
+                    </div>
+                    <div>
+                        <div>{weather && weather.wind_kph} wind</div>
+                        <div>{weather && weather.humidity} humidity</div>
+                    </div>
+                </div>
             </div>
         </div>
         <div className='newsSection'>
-            Newspaper
+            <div>Newspaper</div>
         </div>
     </div>
   )
